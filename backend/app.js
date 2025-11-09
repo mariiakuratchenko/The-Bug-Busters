f1a894aa2549618040493addd3ff18407712adbb
fzn5w <71239140+fzn5w@users.noreply.github.com>
2025-11-09 00:39:19 -0500

    moved dotenv to top
    Removed duplicate dotenv configuration.

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 //The-Bug-Busters\backend\app.js
 
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
 
 const app = express();
 app.use(express.json());
