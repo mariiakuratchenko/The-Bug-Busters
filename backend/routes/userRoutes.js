e0ac8f0d318b1a16ea3a1653cc568b9fab8d1f75
Geti Rahmanoghli <grahmano@my.centennialcollege.ca>
2025-11-09 22:47:50 -0500

    changed location of env

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { getAllUsers, getUserById } = require("../controllers/userController");
-const auth = require("../middleware/authjs");
+const auth = require("../middleware/auth");
 
 router.get("/", auth, getAllUsers);
 router.get("/:id", auth, getUserById);
