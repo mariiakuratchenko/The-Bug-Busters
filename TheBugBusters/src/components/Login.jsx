de6977a61193f0ccc0b780bd911d5c578cc3ef72
Jayani Yogarajah <jyogara3@my.centennialcollege.ca>
2025-11-21 19:26:51 -0500

    textboxes for Login page

--- TheBugBusters/src/components/Login.jsx
+++ TheBugBusters/src/components/Login.jsx
@@ -4,7 +4,42 @@ import React from 'react';
 function Login(){
     return(
         <div>
-            <p>Login!</p>
+            {/* Container for Login Form */}
+            <div className="container">
+            <div className="row">
+                <div className="offset-md-3 col-md-6">
+                    <h1>Signin</h1>
+                    {/* Login Form */}
+                    <form>
+
+                        <div>
+                            {/* Email Text Field */}
+                            <label>Email</label>
+                            <input>
+                            </input>
+                        </div>
+                        <br />
+                        <div>
+                            {/* Password Text Field */}
+                            <label>Password</label>
+                            <input>
+                            </input>
+                        </div>
+                        <br />
+                        {/* Submit Button */}
+                        <button type="submit">
+                            Submit
+                        </button>
+                        {/* Google Signin Button */}
+                        <button type="button">
+                            Sign in with Google
+                        </button>
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
         </div>
     )
 }
