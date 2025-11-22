fd07c865e4b5c79956443fee6be86a30ee58e247
Jayani Yogarajah <jyogara3@my.centennialcollege.ca>
2025-11-21 19:53:52 -0500

    organized signin and register

--- TheBugBusters/src/components/auth/Login.jsx
+++ TheBugBusters/src/components/auth/Login.jsx
@@ -4,42 +4,34 @@ import React from 'react';
 function Login(){
     return(
         <div>
-            {/* Container for Login Form */}
-            <div>
+            <h1>Login</h1>
+            {/* Login Form */}
+            <form>
+
                 <div>
-                    <div>
-                        <h1>Login</h1>
-                        {/* Login Form */}
-                        <form>
-
-                            <div>
-                                {/* Email Text Field */}
-                                <label>Email</label>
-                                <input>
-                                </input>
-                            </div>
-                            <br />
-                            <div>
-                                {/* Password Text Field */}
-                                <label>Password</label>
-                                <input>
-                                </input>
-                            </div>
-                            <br />
-                            {/* Submit Button */}
-                            <button type="submit">
-                                Submit
-                            </button>
-                            {/* Google Signin Button */}
-                            <button type="button">
-                                Sign in with Google
-                            </button>
-
-                        </form>
-                    </div>
-
+                    {/* Email Text Field */}
+                    <label>Email</label>
+                    <input>
+                    </input>
                 </div>
-            </div>
+                <br />
+                <div>
+                    {/* Password Text Field */}
+                    <label>Password</label>
+                    <input>
+                    </input>
+                </div>
+                <br />
+                {/* Submit Button */}
+                <button type="submit">
+                    Submit
+                </button>
+                {/* Google Signin Button */}
+                <button type="button">
+                    Sign in with Google
+                </button>
+
+            </form>
         </div>
     )
 }
