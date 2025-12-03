ad8c28bacc2582ce936ea1a0e112a5111a990ef0
fzn5w <71239140+fzn5w@users.noreply.github.com>
2025-12-02 21:17:01 -0500

    Update vite.config.js

--- TheBugBusters/vite.config.js
+++ TheBugBusters/vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/",
+//   build: {
+//     manifest: true,
+//     rollupOptions: {
+//       input: "./src/main.jsx",
+//     },
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "./src/main.jsx",
-    },
-  },
-});
+})
